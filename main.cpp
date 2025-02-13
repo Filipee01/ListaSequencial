ab0af727cdf25c18c659fe43f5d8819cf4d793b6
Luiz Filipe Santos De Souza <99333746+Filipee01@users.noreply.github.com>
2025-02-13 13:56:40 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,80 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include "ListaSequencial.h"
+
 using namespace std;
 
-int main() {
+void mostrarMenu() {
+    cout << "\nMenu de Operações:\n";
+    cout << "1. Inserir um elemento\n";
+    cout << "2. Modificar um elemento\n";
+    cout << "3. Remover um elemento\n";
+    cout << "4. Obter um elemento\n";
+    cout << "5. Obter o tamanho da lista\n";
+    cout << "6. Exibir a lista\n";
+    cout << "7. Sair\n";
+    cout << "Escolha uma opção: ";
+}
+
+void executarOperacoes() {
     ListaSequencial lista;
+    int opcao, pos, valor;
 
-    // Testando Inserção
-    lista.inserirElemento(1, 10); 
-    lista.inserirElemento(2, 20); 
-    lista.inserirElemento(3, 30); 
-    lista.exibirLista();
+    while (true) {
+        mostrarMenu();
+        cin >> opcao;
 
-    // Testando Modificação
-    lista.setElemento(2, 25); 
-    lista.exibirLista();
+        switch (opcao) {
+            case 1: // Inserir um elemento
+                cout << "Digite a posição para inserir: ";
+                cin >> pos;
+                cout << "Digite o valor a ser inserido: ";
+                cin >> valor;
+                lista.inserirElemento(pos, valor);
+                break;
+            
+            case 2: // Modificar um elemento
+                cout << "Digite a posição do elemento a ser modificado: ";
+                cin >> pos;
+                cout << "Digite o novo valor: ";
+                cin >> valor;
+                lista.setElemento(pos, valor);
+                break;
 
-    // Testando Remoção
-    lista.removerElemento(2); 
-    lista.exibirLista();
+            case 3: // Remover um elemento
+                cout << "Digite a posição do elemento a ser removido: ";
+                cin >> pos;
+                lista.removerElemento(pos);
+                break;
+            
+            case 4: // Obter um elemento
+                cout << "Digite a posição do elemento a ser obtido: ";
+                cin >> pos;
+                valor = lista.getElemento(pos);
+                if (valor != -1) {
+                    cout << "Valor na posição " << pos << ": " << valor << endl;
+                }
+                break;
 
-    // Testando Tamanho
-    cout << "Tamanho da lista: " << lista.getTamanho() << endl; 
+            case 5: // Obter o tamanho da lista
+                cout << "Tamanho da lista: " << lista.getTamanho() << endl;
+                break;
 
-    // Testando Posições Inválidas
-    lista.setElemento(0, 100); 
-    lista.setElemento(3, 100); 
+            case 6: // Exibir a lista
+                cout << "Lista: ";
+                lista.exibirLista();
+                break;
+            
+            case 7: // Sair
+                cout << "Saindo...\n";
+                return;
+            
+            default:
+                cout << "Opção inválida! Tente novamente.\n";
+        }
+    }
+}
 
+int main() {
+    executarOperacoes();
     return 0;
 }
